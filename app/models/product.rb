2d2d634b3ff08f2a230793fb3def94532fdc02d1
Kristen Sundquist <kristen@csc8.com>
2013-03-29 18:42:12 -0700

    Added line_items functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+	has_many :line_items
+
+	before_destroy :ensure_not_referenced_by_any_line_item
+
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01}
@@ -8,4 +12,14 @@ class Product < ActiveRecord::Base
   	message: 'must be a URL for GIF, JPG, or PNG image.'
   }
   validates_length_of :title, minimum: 10, message: "must be at least 10 characters long"
+
+  private
+
+  	# ensure that there are no line items referencing this product
+  	def ensure_not_referenced_by_any_line_items
+	  	return true if line_items.any?
+
+	  	errors.add(:base, 'Line Items present')
+	  	false
+	  end
 end
