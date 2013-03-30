da1a57b530f43a2d6cdf614b257b8dbea65084c4
Kristen Sundquist <kristen@csc8.com>
2013-03-29 21:56:48 -0700

    Added count to line_items

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130330042701) do
+ActiveRecord::Schema.define(:version => 20130330044128) do
 
   create_table "carts", :force => true do |t|
     t.datetime "created_at", :null => false
